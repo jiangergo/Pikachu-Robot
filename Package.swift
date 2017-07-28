1af12e5f2cb72bd9d199e1f80765a8572e2d82e2
jiangergo <jiangergo@163.com>
2017-07-28 20:50:12 +0800

    swift package init & dependency SlackKit
    Signed-off-by: jiangergo <jiangergo@163.com>

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Pikachu-Robot",
+    dependencies: [
+        .Package(url: "https://github.com/SlackKit/SlackKit.git", majorVersion: 4),
+        ]
+)
